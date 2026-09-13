ttibbals2/ardour

commit 007a638a92328ced2a5d663d9974fb607c4b15b7

introduce concept of "transience" for routes, sources, regions
This could also be called "temporariness" but that has more letters.

Commit also includes some loop-autofication in session state code

libs/ardour/region.cc
@@ -2137,16 +2137,16 @@ Region::get_cue_markers (CueMarkers& cues, bool abs) const
 void
 Region::move_cue_marker (CueMarker const & cm, timepos_t const & region_relative_position)
 {
-	for (SourceList::const_iterator s = _sources.begin (); s != _sources.end(); ++s) {
-		(*s)->move_cue_marker (cm, region_relative_position + start());
+	for (auto const & src : _sources) {
+		src->move_cue_marker (cm, region_relative_position + start());
 	}
 }
 
 void
 Region::rename_cue_marker (CueMarker& cm, std::string const & str)
 {
-	for (SourceList::const_iterator s = _sources.begin (); s != _sources.end(); ++s) {
-		(*s)->rename_cue_marker (cm, str);
+	for (auto const & src : _sources) {
+		src->rename_cue_marker (cm, str);
 	}
 }
 
@@ -2154,8 +2154,8 @@ void
 Region::drop_sources ()
 {
 	PBD::Mutex::Lock lx (_source_list_lock);
-	for (SourceList::const_iterator i = _sources.begin (); i != _sources.end(); ++i) {
-		(*i)->dec_use_count ();
+	for (auto const & src : _sources) {
+		src->dec_use_count ();
 	}
 
 	_sources.clear ();
@@ -2172,11 +2172,11 @@ void
 Region::use_sources (SourceList const & s)
 {
 	PBD::Mutex::Lock lx (_source_list_lock);
-	for (SourceList::const_iterator i = s.begin (); i != s.end(); ++i) {
-		_sources.push_back (*i);
-		(*i)->inc_use_count ();
-		_master_sources.push_back (*i);
-		(*i)->inc_use_count ();
+	for (auto const & src : _sources) {
+		_sources.push_back (src);
+		src->inc_use_count ();
+		_master_sources.push_back (src);
+		src->inc_use_count ();
 	}
 	subscribe_to_source_drop ();
 }
@@ -2621,4 +2621,20 @@ Region::meter() const
 		return std::optional<Temporal::Meter>();
 	}
 	return tmap->meter_at (0);
+}
+
+bool
+Region::transient () const
+{
+	for (auto const & src : _sources) {
+		if (src->transient()) {
+			return true;
+		}
+	}
+	for (auto const & src : _master_sources) {
+		if (src->transient()) {
+			return true;
+		}
+	}
+	return false;
 }

libs/ardour/ardour/presentation_info.h
@@ -41,6 +41,7 @@ namespace Properties {
 	LIBARDOUR_API extern PBD::PropertyDescriptor<uint32_t> color;
 	LIBARDOUR_API extern PBD::PropertyDescriptor<bool> selected;
 	LIBARDOUR_API extern PBD::PropertyDescriptor<bool> trigger_track;
+	LIBARDOUR_API extern PBD::PropertyDescriptor<bool> transient;
 	/* we use this; declared in region.cc */
 	LIBARDOUR_API extern PBD::PropertyDescriptor<bool> hidden;
 }
@@ -147,6 +148,8 @@ class LIBARDOUR_API PresentationInfo : public PBD::Stateful
 		VBMAny      = 0x30000,
 #endif
 
+		Transient   = 0x60000,
+
 		/* special mask to detect out "state" bits */
 		StatusMask = (Hidden | MixbusEditorHidden | TriggerTrack),
 
@@ -186,6 +189,7 @@ class LIBARDOUR_API PresentationInfo : public PBD::Stateful
 
 	void set_color (color_t);
 	void set_hidden (bool yn);
+	void set_transient (bool yn);
 	void set_trigger_track (bool yn);
 	void set_flags (Flag f) { _flags = f; }
 
@@ -194,6 +198,7 @@ class LIBARDOUR_API PresentationInfo : public PBD::Stateful
 	int selection_cnt() const { return _selection_cnt; }
 
 	bool hidden() const { return _flags & Hidden; }
+	bool transient() const { return _flags & Transient; }
 	bool trigger_track () const { return _flags & TriggerTrack; }
 	bool special(bool with_master = true) const { return _flags & ((with_master ? MasterOut : 0)|SurroundMaster|MonitorOut|Auditioner); }
 

libs/ardour/ardour/region.h
@@ -283,6 +283,7 @@ public:
 	bool sync_marked ()      const { return _sync_marked; }
 	bool external ()         const { return _external; }
 	bool import ()           const { return _import; }
+	bool transient ()        const;
 
 	Trimmable::CanTrim can_trim () const;
 

libs/ardour/ardour/source.h
@@ -58,6 +58,7 @@ public:
 		Empty = 0x100, /* used for MIDI only */
 		RF64_RIFF = 0x200,
 		Missing = 0x400, /* used for MIDI only */
+		Transient = 0x800, /* do not save with session */
 	};
 
 	typedef PBD::RWLock::ReaderLock ReaderLock;
@@ -140,6 +141,7 @@ public:
 
 	PBD::RWLock& mutex() { return _lock; }
 	Flag flags() const { return _flags; }
+	bool transient() const { return _flags & Transient; }
 
 	virtual void inc_use_count ();
 	virtual void dec_use_count ();

libs/ardour/ardour/stripable.h
@@ -79,6 +79,7 @@ class LIBARDOUR_API Stripable : public SessionObject,
 	bool is_surround_master() const { return _presentation_info.flags() & PresentationInfo::SurroundMaster; }
 	bool is_main_bus() const { return _presentation_info.flags() & PresentationInfo::MainBus; }
 	bool is_singleton () const { return _presentation_info.flags() & PresentationInfo::Singleton; }
+	bool is_transient () const { return _presentation_info.flags() & PresentationInfo::Transient; }
 	/* no fancy bits set */
 	bool is_normal_route () const {
 		PresentationInfo::Flag f (PresentationInfo::Flag (

libs/ardour/playlist.cc
@@ -32,6 +32,7 @@
 
 #include <glibmm/datetime.h>
 
+#include "pbd/no_state.h"
 #include "pbd/stateful_diff_command.h"
 #include "pbd/strsplit.h"
 #include "pbd/types_convert.h"
@@ -2414,6 +2415,19 @@ Playlist::get_template ()
 XMLNode&
 Playlist::state (bool full_state) const
 {
+	bool empty = true;
+
+	for (auto const & r : regions) {
+		if (!r->transient()) {
+			empty = false;
+			break;
+		}
+	}
+
+	if (empty) {
+		throw no_state();
+	}
+
 	XMLNode* node = new XMLNode (X_("Playlist"));
 
 	node->set_property (X_("id"), id ());
@@ -2440,6 +2454,9 @@ Playlist::state (bool full_state) const
 		node->set_property ("combine-ops", _combine_ops);
 
 		for (auto const & r : regions) {
+			if (r->transient()) {
+				continue;
+			}
 			assert (r->sources ().size () > 0 && r->master_sources ().size () > 0);
 			node->add_child_nocopy (r->get_state ());
 		}

libs/ardour/presentation_info.cc
@@ -58,6 +58,7 @@ namespace ARDOUR {
 		PBD::PropertyDescriptor<uint32_t> order;
 		PBD::PropertyDescriptor<uint32_t> color;
 		PBD::PropertyDescriptor<bool>     trigger_track;
+		PBD::PropertyDescriptor<bool>     transient;
 	}
 }
 
@@ -273,6 +274,22 @@ PresentationInfo::set_hidden (bool yn)
 
 		send_change (PropertyChange (Properties::hidden));
 		send_static_change (PropertyChange (Properties::hidden));
+	}
+}
+
+void
+PresentationInfo::set_transient (bool yn)
+{
+	if (yn != transient()) {
+
+		if (yn) {
+			_flags = Flag (_flags | Transient);
+		} else {
+			_flags = Flag (_flags & ~Transient);
+		}
+
+		send_change (PropertyChange (Properties::transient));
+		send_static_change (PropertyChange (Properties::transient));
 	}
 }
 

libs/ardour/session_state.cc
@@ -80,6 +80,7 @@
 #include "pbd/debug.h"
 #include "pbd/enumwriter.h"
 #include "pbd/error.h"
+#include "pbd/no_state.h"
 #include "pbd/file_utils.h"
 #include "pbd/pathexpand.h"
 #include "pbd/pthread_utils.h"
@@ -1475,10 +1476,19 @@ Session::export_route_state (std::shared_ptr<RouteList> rl, const string& path, 
 		 */
 		child = node->add_child ("Playlists"); // SessionPlaylists::add_state
 		for (PlaylistSet::const_iterator i = playlists.begin(); i != playlists.end(); ++i) {
-			child->add_child_nocopy ((*i)->get_state ());
+
+			try {
+				child->add_child_nocopy ((*i)->get_state ());
+			} catch (no_state const & ns) {
+				continue;
+			}
+
 			std::shared_ptr<RegionList> prl = (*i)->region_list ();
-			for (RegionList::const_iterator s = prl->begin(); s != prl->end(); ++s) {
-				const Region::SourceList& sl = (*s)->sources ();
+			for (auto const & r : *prl) {
+				if (r->transient()) {
+					continue;
+				}
+				const Region::SourceList& sl = r->sources ();
 				for (Region::SourceList::const_iterator sli = sl.begin(); sli != sl.end(); ++sli) {
 					sources.insert (*sli);
 				}
@@ -2013,8 +2023,12 @@ Session::state (bool save_template, snapshot_t snapshot_type, bool for_archive, 
 
 		if (!only_used_assets) {
 			const RegionFactory::RegionMap& region_map (RegionFactory::all_regions());
-			for (RegionFactory::RegionMap::const_iterator i = region_map.begin(); i != region_map.end(); ++i) {
-				std::shared_ptr<Region> r = i->second;
+			for (auto const & [id,r] : region_map) {
+
+				if (r->transient()) {
+					continue;
+				}
+
 				/* regions must have sources */
 				assert (r->sources().size() > 0 && r->master_sources().size() > 0);
 				/* only store regions not attached to playlists */
@@ -2037,7 +2051,10 @@ Session::state (bool save_template, snapshot_t snapshot_type, bool for_archive, 
 			std::set<std::shared_ptr<Region>> tr;
 			{
 				std::shared_ptr<RouteList const> rl = routes.reader();
-				for (auto const& r : *rl) {
+				for (auto const & r : *rl) {
+					if (r->is_transient()) {
+						continue;
+					}
 					std::shared_ptr<TriggerBox> tb = r->triggerbox ();
 					if (tb) {
 						tb->used_regions (tr);
@@ -2047,8 +2064,11 @@ Session::state (bool save_template, snapshot_t snapshot_type, bool for_archive, 
 
 			auto const& used_pl (_playlists->get_used ());
 			const RegionFactory::RegionMap& region_map (RegionFactory::all_regions());
-			for (RegionFactory::RegionMap::const_iterator i = region_map.begin(); i != region_map.end(); ++i) {
-				std::shared_ptr<Region> r = i->second;
+			for (auto const & [id,r] : region_map) {
+
+				if (r->transient()) {
+					continue;
+				}
 
 				if (tr.find (r) != tr.end()) {
 					child->add_child_nocopy (r->get_state ());
@@ -2155,6 +2175,11 @@ Session::state (bool save_template, snapshot_t snapshot_type, bool for_archive, 
 		xml_node_order.sort (cmp);
 
 		for (RouteList::const_iterator i = xml_node_order.begin(); i != xml_node_order.end(); ++i) {
+
+			if ((*i)->is_transient()) {
+				continue;
+			}
+
 			if (!(*i)->is_auditioner()) {
 				if (save_template) {
 					child->add_child_nocopy ((*i)->get_template());

commit 9f3abaa217e81284a076bf656a0f78864a1cec6c

fix error introduced in 007a638a92 during auto-fication

libs/ardour/region.cc
@@ -2172,7 +2172,7 @@ void
 Region::use_sources (SourceList const & s)
 {
 	PBD::Mutex::Lock lx (_source_list_lock);
-	for (auto const & src : _sources) {
+	for (auto const & src : s) {
 		_sources.push_back (src);
 		src->inc_use_count ();
 		_master_sources.push_back (src);